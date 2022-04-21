1069de290a09333e4a4091236fd355cb22be70ac
v-aguiar <v.aguiar.dev@gmail.com>
2022-04-21 16:39:55 -0300

    Set node express app to listen on port 5000 :deploy:

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿import express from "express";
+import cors from "cors";
+import chalk from "chalk";
+
+const app = express();
+app.use(cors());
+
+app.listen(5000, () =>
+  console.log(
+    chalk.magentaBright.bold(
+      "\n🚀 Server is runinng!!!\n\nListening on port 5000...\n"
+    )
+  )
+);
